1f5c9aefadb054afc2b57d3985f09487ef05bc07
Yuki Suwabe <ys462@cornell.edu>
2023-05-20 17:12:56 -0400

    Fixed description for THey Called Us Enemy

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export const Home = () => {
         <div class='intro'>
           <img class='bookcover' src={calledusCover} alt="They Called Us Enemy Cover" />
           <div class='bookdesc'>
-            <p class='desc'> Baseball Saved Us is a picture book that features a young boy who gets sent to the Incarceration camp and learns to play baseball and connects with the people around him through baseball.</p>
+            <p class='desc'> They Called Us Enemy is a graphic novel of George Takei’s story of how he went through the incarceration camps at a young age and how their family and the people around them dealt through the events. </p>
             <a class='desc' href='/baseballsavedus'>Link to article</a>
           </div>
         </div>
